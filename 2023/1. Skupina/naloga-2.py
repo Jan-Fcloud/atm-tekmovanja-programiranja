22be66009962fcb27ed3abe6f990f1323a89da10
Jan <jan@jans.dev>
2024-01-09 12:21:01 +0100

    2023 - 1.s - n.2 (dodati se je treba komentarje)

--- 2023/1. Skupina/naloga-2.py
+++ 2023/1. Skupina/naloga-2.py
@@ -33,3 +33,69 @@ Primeri:
 | 128975000 | 124 MB   |
 
 """
+
+#######################################################
+# Naslednja rešitev je narejena z uporabo podprogramom.
+#######################################################
+
+def izpisi_velikost(velikost): # Definiramo glavni podprogram, ki bo pravilno izpisal velikost.
+    ostanek = 0 # Definiramo ostanek ki nam bo pomagal pri popolnem izpisu velikosti ki se usklaja z nalogo.
+
+    #########################################################
+    # V podprogramu imamo vsepovsod v if stavkih "< 1", saj
+    # je v nalogi navedeno če je rezultat več kot 4 številčni,
+    # se naj izpiše v preponi ki sledi trenutni.
+
+    if velikost < 10000:
+        return str(velikost) + " B"
+    else:
+        ostanek = velikost % 1024
+        velikost /= 1024
+
+        if velikost < 10000:
+            return str(round(velikost) + ostanek) + " KB"
+        else:
+            ostanek = ostanekPlus(velikost % 1024)
+            velikost /= 1024
+            if velikost < 10000:
+                return str(round(velikost) + ostanek) + " MB"
+            else:
+                ostanek = ostanekPlus(velikost % 1024)
+                velikost /= 1024
+                if velikost < 10000:
+                    return str(round(velikost) + ostanek) + " GB"
+                else:
+                    ostanek = ostanekPlus(velikost % 1024)
+                    velikost /= 1024
+                    if velikost < 10000:
+                        return str(round(velikost) + ostanek) + " TB"
+                    else:
+                        ostanek = ostanekPlus(velikost % 1024)
+                        velikost /= 1024
+                        if velikost < 10000:
+                            return str(round(velikost) + ostanek) + " PB"
+                        else:
+                            return str(round(velikost) + ostanek) + " EB"
+
+    
+
+# Uporaba naslednjega podprograma je da se odločimo če bomo pripisali rezultatu 1 ali ne.
+# V navodilih naloge je napisano naslednje:
+"""
+Če število bajtov ni mnogokratnik vrednosti predpone (in bi pri deljenju ostale decimalke), zaokrožimo število navzgor, npr. 234,03 KB izpišemo kot 235 KB, 234,00 KB
+pa kot 234 KB.
+"""
+# Glede tega sklepamo da je pravilno da zaokrožimo navzgor, če je ostanek večji od 0.
+# Vrnjen rezultat je seštet k izpisu velikosti v glavnem podprogramu.
+def ostanekPlus(dec):
+    if dec > 0: # Če je ostanek večji od 0, vrnemo 1, drugače 0.
+        return 1
+    else:
+        return 0
+
+
+def main():
+    velikost = int(input("Vnesi velikost datoteke: "))
+    print(izpisi_velikost(velikost))
+
+main()
